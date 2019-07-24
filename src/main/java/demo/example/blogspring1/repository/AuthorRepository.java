4488aa0c745ff979ca7169ee6147d16ffd91ab70
User <mi2miag2016@gmail.com>
2019-07-24 12:22:11 -0700

    first commit

--- src/main/java/demo/example/blogspring1/repository/AuthorRepository.java
+++ src/main/java/demo/example/blogspring1/repository/AuthorRepository.java
@@ -2,6 +2,9 @@ package demo.example.blogspring1.repository;
 
 import demo.example.blogspring1.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface AuthorRepository extends JpaRepository<Author,Long> {
 }
--- src/main/java/demo/example/blogspring1/repository/PostRepository.java
+++ src/main/java/demo/example/blogspring1/repository/PostRepository.java
@@ -2,6 +2,9 @@ package demo.example.blogspring1.repository;
 
 import demo.example.blogspring1.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface PostRepository extends JpaRepository<Post,Long> {
 }
